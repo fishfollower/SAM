493d326d9b5e10f36fbfca25eb5772ead0ad89a8
Anders Nielsen <fishfollower@gmail.com>
2016-09-09 16:01:46 +0200

    Addid a few missing files

---
+++ SAM/R/init.R
@@ -0,0 +1,22 @@
+##' Setup minimal initial parameters
+##' @param dat sam data object 
+##' @details ...
+##' @export
+defpar <- function(dat,conf){
+  ret<-list()
+  ret$logFpar=numeric(max(conf$keyLogFpar)+1)-5 
+  ret$logQpow=numeric(max(conf$keyQpow)+1)
+  ret$logSdLogFsta=numeric(max(conf$keyVarF)+1)-.7
+  ret$logSdLogN=numeric(max(conf$keyVarLogN)+1)-.35
+  ret$logSdLogObs=numeric(max(conf$keyVarObs)+1)-.35
+  ret$rec_loga=if(conf$stockRecruitmentModelCode==0){numeric(0)}else{numeric(1)}
+  ret$rec_logb=if(conf$stockRecruitmentModelCode==0){numeric(0)}else{numeric(1)} 
+  ret$logit_rho=numeric(1)+.5
+  ret$logScale=numeric(conf$noScaledYears)
+  ret$logScaleSSB=if(any(dat$fleetTypes%in%c(3,4))){numeric(1)}else{numeric(0)}
+  ret$logPowSSB=if(any(dat$fleetTypes==4)){numeric(1)}else{numeric(0)}
+  ret$logSdSSB=if(any(dat$fleetTypes%in%c(3,4))){numeric(1)}else{numeric(0)}
+  ret$logF=matrix(0, nrow=max(conf$keyLogFsta)+1,ncol=dat$noYears)
+  ret$logN=matrix(0, nrow=conf$maxAge-conf$minAge+1, ncol=dat$noYears)
+  return(ret)
+}
