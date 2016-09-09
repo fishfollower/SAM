bf655b7fdba84b905a216890fa652c8c0effd331
Anders Nielsen <fishfollower@gmail.com>
2016-09-09 10:44:19 +0200

    Added a basic test script

---
+++ tests/bluewhiting/script.R
@@ -0,0 +1,14 @@
+library(SAM)
+
+cn<-read.ices("cn.dat")
+cw<-read.ices("cw.dat")
+dw<-read.ices("dw.dat")
+lf<-read.ices("lf.dat")
+lw<-read.ices("lw.dat")
+mo<-read.ices("mo.dat")
+pf<-read.ices("pf.dat")
+pm<-read.ices("pm.dat")
+sw<-read.ices("sw.dat")
+surveys<-surveys.ices("survey.dat")
+
+
