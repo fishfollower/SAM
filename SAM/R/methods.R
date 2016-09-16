8f2d2584b3cf8e6578c6ac2ae42353f251058f36
Anders Nielsen <fishfollower@gmail.com>
2016-09-16 21:07:39 +0200

    logLik and print method

--- SAM/R/methods.R
+++ SAM/R/methods.R
@@ -9,3 +9,22 @@ plot.sam<-function(fit,...){
   recplot(fit,...)
   par(mfrow=c(1,1))
 }
+
+##' Print SAM fit 
+##' @param  fit ... 
+##' @details ...
+##' @export
+print.sam<-function(fit,...){
+ cat("SAM model: log likelihood is", logLik(fit,...),"Convergence", ifelse(0==fit$opt$convergence, "OK\n", "failed\n"))
+}
+
+##' Log likelihood SAM fit 
+##' @param  fit ... 
+##' @details ...
+##' @export
+logLik.sam<-function(fit,...){
+ ret<- -fit$opt$objective
+ attr(ret,"df")<-length(fit$opt$par)
+ class(ret)<-"logLik"
+ ret
+}
