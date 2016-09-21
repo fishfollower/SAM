56f3d69c3cd5819252143974eb8a544aefe87b83
Anders Nielsen <fishfollower@gmail.com>
2016-09-21 16:31:34 +0200

    more tables

--- SAM/R/tables.R
+++ SAM/R/tables.R
@@ -1,13 +1,40 @@
+##' Table helper 
+##' @param  fit ... 
+##' @details ...
+.tableit <-function (what, x=fit$data$years, ylab=what, trans=function(x)x ,...){
+   idx<-names(fit$sdrep$value)==what
+   y<-fit$sdrep$value[idx]
+   ci<-y+fit$sdrep$sd[idx]%o%c(-2,2)
+   ret<-trans(cbind(y,ci))
+   rownames(ret)<-x
+   colnames(ret)<-c("Estimate","Low","Hig")
+   return(ret)
+ }
+
 ##' SSB table 
 ##' @param  fit ... 
 ##' @details ...
 ##' @export
 ssbtable<-function(fit,...){
-   idx<-names(fit$sdrep$value)=="logssb"
-   y<-fit$sdrep$value[idx]
-   ci<-y+fit$sdrep$sd[idx]%o%c(-2,2)
-   ret<-cbind(y,ci)
-   rownames(ret)<-fit$data$years
-   colnames(ret)<-c("Estimate","Low","Hig")
-   return(exp(ret))
+   ret<-.tableit("logssb", trans=exp, ...) 
+   return(ret)
 }
+
+##' Fbar table 
+##' @param  fit ... 
+##' @details ...
+##' @export
+fbartable<-function(fit,...){
+   ret<-.tableit("logfbar", trans=exp, ...) 
+   return(ret)
+}
+
+##' Recruit table 
+##' @param  fit ... 
+##' @details ...
+##' @export
+rectable<-function(fit,...){
+   ret<-.tableit("logR", trans=exp, ...) 
+   return(ret)
+}
+
