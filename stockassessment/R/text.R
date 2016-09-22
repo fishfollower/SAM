5234a5800aa0331e898f05589dcb4e8227769016
Anders Nielsen <fishfollower@gmail.com>
2016-09-22 15:38:46 +0200

    placeholder for the model description

---
+++ stockassessment/R/text.R
@@ -0,0 +1,12 @@
+##' Description of model  
+##' @param fit returned object from sam.fit
+##' @param ... Additional parameters to be passed to ...
+##' @importFrom utils packageVersion
+##' @details ...
+modelDescription <-function (fit,...){
+   ret<-list()
+   ret$modelVersion<-paste('The model is a state-space stock assessment (SAM from the package "stockassessment" version ',
+                           packageVersion("stockassessment"), ".", sep="")
+   ret$modelIntuition<-'The model works by assuming that stock-sizes at age (N) and fishing mortalities (F) are unobserved ...' 
+   cat(ret$modelVersion,ret$modelIntuition)
+}
