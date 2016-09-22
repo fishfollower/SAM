f7fbcca714208a1c0e379b6635c05ddeac84ea11
Anders Nielsen <fishfollower@gmail.com>
2016-09-22 17:05:18 +0200

    ...

--- stockassessment/R/text.R
+++ stockassessment/R/text.R
@@ -3,6 +3,7 @@
 ##' @param ... Additional parameters to be passed to ...
 ##' @importFrom utils packageVersion
 ##' @details ...
+##' @export
 modelDescription <-function (fit,...){
    ret<-list()
    ret$modelVersion<-paste('The model is a state-space stock assessment (SAM from the package "stockassessment" version ',
