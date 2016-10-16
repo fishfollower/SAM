28950f041f5f3b6a26c918d2f43c62aa6ec40cf7
Anders Nielsen <fishfollower@gmail.com>
2016-10-16 20:27:31 +0200

    model des

--- stockassessment/R/text.R
+++ stockassessment/R/text.R
@@ -5,9 +5,21 @@
 ##' @details ...
 ##' @export
 modelDescription <-function (fit,...){
-   ret<-list()
-   ret$modelVersion<-paste('The model is a state-space stock assessment (SAM from the package "stockassessment" version ',
-                           packageVersion("stockassessment"), ".", sep="")
-   ret$modelIntuition<-'The model works by assuming that stock-sizes at age (N) and fishing mortalities (F) are unobserved ...' 
-   cat(ret$modelVersion,ret$modelIntuition)
+  "+" = function(x,y) {
+      if(is.character(x) || is.character(y)) {
+        return(paste(x , y, sep=""))
+      } else {
+        .Primitive("+")(x,y)
+      }
+  } 
+  ret<-list()
+  ret$modelVersion <- 'The model is a state-space stock assessment (SAM from the package "stockassessment" version ' +
+                      packageVersion("stockassessment") + ").\n\n"
+  ret$modelIntro <- 'The model works by assuming that stock-sizes at age (N) and fishing mortalities at age (F) ' +
+                      'are unobserved processes.'
+  ret$ages <-  'The first age group is age ' + fit$conf$minAge + ' and the last age group is age ' + fit$conf$maxAge +
+                      ifelse(fit$conf$maxAgePlusGroup==1,'+. ','. ') 
+  ret$data <- 'The data period covers ' + fit$data$noYears + ' years (from ' + min(fit$data$years) + ' to ' +
+              max(fit$data$years) + '). ' 
+  cat(ret$modelVersion+ret$modelIntro+ret$ages+ret$data)
 }
