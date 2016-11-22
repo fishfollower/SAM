b73ad0835715ab61174add4e0d94a25ba5d51cb0
Anders Nielsen <fishfollower@gmail.com>
2016-11-22 12:13:58 +0100

    updated conf file

--- stockassessment/R/conf.R
+++ stockassessment/R/conf.R
@@ -4,9 +4,9 @@
 ##' @details ...
 setSeq<-function(min,max){
   if(min==max){
-    ret<-1
+    ret <- 1
   }else{
-    ret<-c(1:(max-min),max-min)
+    ret <- c(1:(max-min),max-min)
   }
   return(ret)
 }
@@ -16,52 +16,54 @@ setSeq<-function(min,max){
 ##' @details ...
 ##' @export
 defcon<-function(dat){
-  fleetTypes<-dat$fleetTypes
-  ages<-do.call(rbind,tapply(dat$aux[,3], INDEX=dat$aux[,2], FUN=range))
-  minAge<-min(ages)
-  maxAge<-max(ages)
-  nAges<-maxAge-minAge+1
-  nFleets<-nrow(ages)
-  ret<-list()
+  fleetTypes <- dat$fleetTypes
+  ages <- do.call(rbind,tapply(dat$aux[,3], INDEX=dat$aux[,2], FUN=range))
+  ages[fleetTypes%in%c(3,5),] <- NA
+  minAge <- min(ages, na.rm=TRUE)
+  maxAge <- max(ages, na.rm=TRUE)
+  ages[is.na(ages)] <- minAge
+  nAges <- maxAge-minAge+1
+  nFleets <- nrow(ages)
+  ret <- list()
   ret$minAge <- minAge
   ret$maxAge <- maxAge  
   ret$maxAgePlusGroup <- 1
-  x<-matrix(0, nrow=nFleets, ncol=nAges)
-  lastMax<-0
+  x <- matrix(0, nrow=nFleets, ncol=nAges)
+  lastMax <- 0
   for(i in 1:nrow(x)){
     if(fleetTypes[i]==0){
-      x[i,(ages[i,1]-minAge+1):(ages[i,2]-minAge+1)]<-setSeq(ages[i,1],ages[i,2])+lastMax
-      lastMax<-max(x)
+      x[i,(ages[i,1]-minAge+1):(ages[i,2]-minAge+1)] <- setSeq(ages[i,1],ages[i,2])+lastMax
+      lastMax <- max(x)
     }
   }  
   ret$keyLogFsta <- x - 1
   ret$corFlag <- 0
-  x<-matrix(0, nrow=nFleets, ncol=nAges)
-  lastMax<-0
+  x <- matrix(0, nrow=nFleets, ncol=nAges)
+  lastMax <- 0
   for(i in 1:nrow(x)){
-    if(fleetTypes[i]%in%c(1,2)){
-      x[i,(ages[i,1]-minAge+1):(ages[i,2]-minAge+1)]<-setSeq(ages[i,1],ages[i,2])+lastMax
-      lastMax<-max(x)
+    if(fleetTypes[i]%in%c(1,2,3)){
+      x[i,(ages[i,1]-minAge+1):(ages[i,2]-minAge+1)] <- setSeq(ages[i,1],ages[i,2])+lastMax
+      lastMax <- max(x)
     }
   }  
   ret$keyLogFpar <- x - 1 
   ret$keyQpow <- matrix(-1, nrow=nFleets, ncol=nAges)
   x<-matrix(0, nrow=nFleets, ncol=nAges)
-  lastMax<-0
+  lastMax <- 0
   for(i in 1:nrow(x)){
     if(fleetTypes[i]==0){
       x[i,(ages[i,1]-minAge+1):(ages[i,2]-minAge+1)]<-lastMax+1
-      lastMax<-max(x)
+      lastMax <- max(x)
     }
   }  
   ret$keyVarF <- x - 1
   ret$keyVarLogN <- c(1,rep(2,nAges-1)) - 1 
-  x<-matrix(0, nrow=nFleets, ncol=nAges)
-  lastMax<-0
+  x <- matrix(0, nrow=nFleets, ncol=nAges)
+  lastMax <- 0
   for(i in 1:nrow(x)){
-    if(fleetTypes[i]%in%c(0,1,2)){
-      x[i,(ages[i,1]-minAge+1):(ages[i,2]-minAge+1)]<-lastMax+1
-      lastMax<-max(x)
+    if(fleetTypes[i]%in%c(0,1,2,3)){
+      x[i,(ages[i,1]-minAge+1):(ages[i,2]-minAge+1)] <- lastMax+1
+      lastMax <- max(x)
     }
   }  
   ret$keyVarObs <- x - 1
