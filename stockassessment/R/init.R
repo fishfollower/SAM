a6c17b26db5db94cdffbe9485208cb9cbf0505f7
Anders Nielsen <fishfollower@gmail.com>
2016-11-22 12:15:18 +0100

    updated init.R

--- stockassessment/R/init.R
+++ stockassessment/R/init.R
@@ -18,9 +18,6 @@ defpar <- function(dat,conf){
   ret$rec_logb=if(conf$stockRecruitmentModelCode==0){numeric(0)}else{numeric(1)} 
   ret$itrans_rho=if(conf$corFlag==0){numeric(0)}else{numeric(1)+.5}
   ret$logScale=numeric(conf$noScaledYears)
-  ret$logScaleSSB=if(any(dat$fleetTypes%in%c(3,4))){numeric(1)}else{numeric(0)}
-  ret$logPowSSB=if(any(dat$fleetTypes==4)){numeric(1)}else{numeric(0)}
-  ret$logSdSSB=if(any(dat$fleetTypes%in%c(3,4))){numeric(1)}else{numeric(0)}
   ret$logF=matrix(0, nrow=max(conf$keyLogFsta)+1,ncol=dat$noYears)
   ret$logN=matrix(0, nrow=conf$maxAge-conf$minAge+1, ncol=dat$noYears)
   return(ret)
