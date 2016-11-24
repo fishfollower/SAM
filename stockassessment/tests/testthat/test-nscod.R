5066b375eea25ff950842111a17476898f96ffe5
mollie brooks <mollieebrooks@gmail.com>
2016-11-24 13:28:41 +0100

    changed tests and Makefie so that 'make test' works now

--- stockassessment/tests/testthat/test-nscod.R
+++ stockassessment/tests/testthat/test-nscod.R
@@ -1,54 +1,10 @@
-stopifnot(require("testthat"),
-		require("stockassessment"))
-
 context("nscod example")
 setwd("../nscod/")
 
-cn<-read.ices("cn.dat")
-cw<-read.ices("cw.dat")
-dw<-read.ices("dw.dat")
-lf<-read.ices("lf.dat")
-lw<-read.ices("lw.dat")
-mo<-read.ices("mo.dat")
-nm<-read.ices("nm.dat")
-pf<-read.ices("pf.dat")
-pm<-read.ices("pm.dat")
-sw<-read.ices("sw.dat")
-surveys<-read.ices("survey.dat")
-
-dat<-setup.sam.data(surveys=surveys,
-                    residual.fleet=cn, 
-                    prop.mature=mo, 
-                    stock.mean.weight=sw, 
-                    catch.mean.weight=cw, 
-                    dis.mean.weight=dw, 
-                    land.mean.weight=lw,
-                    prop.f=pf, 
-                    prop.m=pm, 
-                    natural.mortality=nm, 
-                    land.frac=lf)
-
-conf<-defcon(dat)
-conf$keyLogFsta[1,] <- c(0, 1, 2, 3, 4, 5)
-conf$corFlag <- 2
-conf$keyLogFpar <- matrix(
-         c(-1, -1, -1, -1, -1, -1,
-            0,  1,  2,  3,  4, -1,
-            5,  6,  7,  8, -1, -1
-           ), nrow=3, byrow=TRUE)
-conf$keyVarF[1,] <- c(0, 1, 1, 1, 1, 1)
-conf$keyVarObs <- matrix(
-         c( 0,  1,  2,  2,  2,  2,
-            3,  4,  4,  4,  4, -1,
-            5,  6,  6,  6, -1, -1
-           ), nrow=3, byrow=TRUE)
-conf$noScaledYears <- 13
-conf$keyScaledYears <- 1993:2005
-conf$keyParScaledYA <- row(matrix(NA, nrow=13, ncol=6))-1
-conf$fbarRange <- c(2,4)
-
-par<-defpar(dat,conf)
-fit<-sam.fit(dat,conf,par)
+stopifnot(require("testthat"),
+		require("stockassessment"))
+source("../compare.fits.R")
+source("script.R")
 load("fit.expected.Rdata")#reads in fit.exp
 compare.fits(fit, fit.exp)
 
