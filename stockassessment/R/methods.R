92cd27516230b4dfbedeafeaa722d72496f5c73e
mollie brooks <mollieebrooks@gmail.com>
2016-12-05 11:21:16 +0100

    standardized code indent and improved simulate()

--- stockassessment/R/methods.R
+++ stockassessment/R/methods.R
@@ -54,7 +54,7 @@ plot.samres<-function(x, ...){
 ##' @details ...
 ##' @export
 print.sam<-function(x, ...){
- cat("SAM model: log likelihood is", logLik.sam(x,...),"Convergence", ifelse(0==x$opt$convergence, "OK\n", "failed\n"))
+  cat("SAM model: log likelihood is", logLik.sam(x,...),"Convergence", ifelse(0==x$opt$convergence, "OK\n", "failed\n"))
 }
 
 ##' Print samres object 
@@ -64,8 +64,8 @@ print.sam<-function(x, ...){
 ##' @details ...
 ##' @export
 print.samres<-function(x, ...){
- class(x)<-NULL
- print(as.data.frame(x))
+  class(x)<-NULL
+  print(as.data.frame(x))
 }
 
 ##' Log likelihood of sam object 
@@ -75,10 +75,10 @@ print.samres<-function(x, ...){
 ##' @details ...
 ##' @export
 logLik.sam<-function(object, ...){
- ret<- -object$opt$objective
- attr(ret,"df")<-length(object$opt$par)
- class(ret)<-"logLik"
- ret
+  ret<- -object$opt$objective
+  attr(ret,"df")<-length(object$opt$par)
+  class(ret)<-"logLik"
+  ret
 }
 
 ##' Extract fixed coefficients of sam object 
@@ -89,11 +89,11 @@ logLik.sam<-function(object, ...){
 ##' @importFrom stats coef
 ##' @export
 coef.sam <- function(object, ...){
- ret <- object$sdrep$par.fixed
- attr(ret,"cov") <- object$sdrep$cov.fixed
- attr(ret,"sd") <- sqrt(diag(object$sdrep$cov.fixed))
- class(ret)<-"samcoef"
- ret
+  ret <- object$sdrep$par.fixed
+  attr(ret,"cov") <- object$sdrep$cov.fixed
+  attr(ret,"sd") <- sqrt(diag(object$sdrep$cov.fixed))
+  class(ret)<-"samcoef"
+  ret
 }
 
 ##' Print samcoef object 
@@ -103,9 +103,9 @@ coef.sam <- function(object, ...){
 ##' @details ...
 ##' @export
 print.samcoef<-function(x, ...){
- y<-as.vector(x)
- names(y)<-names(x)
- print(y)
+  y<-as.vector(x)
+  names(y)<-names(x)
+  print(y)
 }
 
 
@@ -117,7 +117,7 @@ print.samcoef<-function(x, ...){
 ##' @details ...
 ##' @export
 nobs.sam<-function(object, ...){
- as.integer(object$data$nobs)
+  as.integer(object$data$nobs)
 }
 
 ##' Extract residuals from sam object 
@@ -130,10 +130,10 @@ nobs.sam<-function(object, ...){
 ##' @details ...
 ##' @export
 residuals.sam<-function(object, discrete=FALSE, ...){
- res <- oneStepPredict(object$obj, observation.name="logobs", data.term.indicator="keep", discrete=discrete,...)
- ret <- cbind(object$data$aux, res)
- class(ret)<-"samres"
- ret
+  res <- oneStepPredict(object$obj, observation.name="logobs", data.term.indicator="keep", discrete=discrete,...)
+  ret <- cbind(object$data$aux, res)
+  class(ret)<-"samres"
+  ret
 }
 
 ##' Summary of sam object 
@@ -143,11 +143,11 @@ residuals.sam<-function(object, discrete=FALSE, ...){
 ##' @details ...
 ##' @export
 summary.sam<-function(object, ...){
- ret <- cbind(round(rectable(object)), round(ssbtable(object)), round(fbartable(object),2))
- colnames(ret)[1] <- paste("R(age ", object$conf$minAge, ")", sep="") 
- colnames(ret)[4] <- "SSB"
- colnames(ret)[7] <- paste("Fbar(",object$conf$fbarRange[1], "-", object$conf$fbarRange[2], ")", sep="")
- ret
+  ret <- cbind(round(rectable(object)), round(ssbtable(object)), round(fbartable(object),2))
+  colnames(ret)[1] <- paste("R(age ", object$conf$minAge, ")", sep="") 
+  colnames(ret)[4] <- "SSB"
+  colnames(ret)[7] <- paste("Fbar(",object$conf$fbarRange[1], "-", object$conf$fbarRange[2], ")", sep="")
+  ret
 }
 
 ##' Simulate from a sam object 
@@ -155,17 +155,21 @@ summary.sam<-function(object, ...){
 ##' @param object sam fitted object (result from sam.fit)
 ##' @param nsim number of response lists to simulate. Defaults to 1.
 ##' @param seed random number seed
+##' @param full.data logical, should each inner list contain a full list of data. Defaults to TRUE  
 ##' @param ... extra arguments
 ##' @importFrom stats simulate
 ##' @details ...
 ##' @return returns a list of lists. The outer list has length \code{nsim}. Each inner list contains simulated values of \code{logF}, \code{logN}, and \code{obs} with dimensions equal to those parameters.
 ##' @export
-simulate.sam<-function(object, nsim=1, seed=NULL, ...){
- ret <- list()
- if(!is.null(seed)) set.seed(seed)
- for (i in 1:nsim)
- {
-   ret[[i]] <- object$obj$simulate()
- }
- ret
+simulate.sam<-function(object, nsim=1, seed=NULL, full.data=TRUE, ...){
+  if(!is.null(seed)) set.seed(seed)
+  if(full.data){
+    ret <- replicate(nsim, 
+    	c(object$data[names(object$data)!="logobs"],#all the old data
+    	object$obj$simulate()["logobs"])#simulated observations
+    	, simplify=FALSE)
+  }else{
+  	ret <- replicate(nsim, object$obj$simulate(), simplify=FALSE)
+  }
+  ret
 }
