0119c32c709066d29ad20ef7c612f28f56b5f68d
Anders Nielsen <fishfollower@gmail.com>
2017-04-25 12:35:01 +0200

    forecast stuff

--- stockassessment/R/forecast.R
+++ stockassessment/R/forecast.R
@@ -6,13 +6,14 @@
 ##' @param nosim number of simulations default is 1000
 ##' @param year.base starting year default last year in assessment. Currently it is only supported to use last assessment year or the year before  
 ##' @param ave.years vector of years to average for weights, maturity, M and such  
-##' @param rec.years vector of years to use to resample recruitment from 
+##' @param rec.years vector of years to use to resample recruitment from
+##' @param label optional label to appear in short table
 ##' @details There are three ways to specify a scenario. If e.g. four F values are specified (e.g. fval=c(.1,.2,.3,4)), then the first value is used in the last assessment year (base.year), and the three following in the three following years. Alternatively F's can be specified by a scale, or a target catch. Only one option can be used per year. So for instance to set a catch in the first year and an F-scale in the following one would write catchval=c(10000,NA,NA,NA), fscale=c(NA,1,1,1). The length of the vector specifies how many years forward the scenarios run. 
 ##' @return an object of type samforecast
 ##' @importFrom stats median uniroot quantile
 ##' @importFrom MASS mvrnorm
 ##' @export
-forecast <- function(fit, fscale=NULL, catchval=NULL, fval=NULL, nosim=1000, year.base=max(fit$data$years), ave.years=max(fit$data$years)+(-4:0), rec.years=max(fit$data$years)+(-9:0)){
+forecast <- function(fit, fscale=NULL, catchval=NULL, fval=NULL, nosim=1000, year.base=max(fit$data$years), ave.years=max(fit$data$years)+(-4:0), rec.years=max(fit$data$years)+(-9:0), label=NULL){
 
   if(missing(fscale)&missing(fval)&missing(catchval))stop("No scenario is specified")    
   if(missing(fscale)&!missing(fval))fscale<-rep(NA,length(fval))
@@ -222,7 +223,10 @@ forecast <- function(fit, fscale=NULL, catchval=NULL, fval=NULL, nosim=1000, yea
   nam <- c("median","low","high")
   colnames(tab) <- paste0(rep(c("fbar:","rec:","ssb:","catch:"), each=length(nam)), nam)
   attr(simlist, "tab")<-tab
-    
+  shorttab<-t(tab[,grep("median",colnames(tab))])
+  rownames(shorttab)<-sub(":median","",paste0(label,if(!is.null(label))":",rownames(shorttab)))
+  attr(simlist, "shorttab")<-shorttab
+  attr(simlist, "label") <- label  
   class(simlist) <- "samforecast"
   simlist
 }
