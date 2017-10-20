938f6426440a67373e73b152ef3e7c682bfea4e4
Anders Nielsen <fishfollower@gmail.com>
2017-10-20 14:09:59 +0200

    fix colnames

--- stockassessment/R/conf.R
+++ stockassessment/R/conf.R
@@ -143,7 +143,7 @@ saveConf <- function(x, file="", overwrite=FALSE){
 
   writeConf.matrix <- function(x,...){
     if(nrow(x)>0){
-      cat(capture.output(prmatrix(x, rowlab=rep("", nrow(x)), collab=rep("",ncol(x)))), sep="\n", ...)
+      cat(capture.output(prmatrix(x, rowlab=rep("", nrow(x)), collab=rep("   ",ncol(x)))), sep="\n", ...)
     }else{
       cat("\n", ...)
     }
@@ -176,7 +176,8 @@ saveConf <- function(x, file="", overwrite=FALSE){
     txt$keyVarLogN <- "Coupling of process variance parameters for log(N)-process"
     txt$keyVarObs <- "Coupling of the variance parameters for the observations."
     txt$obsCorStruct <- "Covariance structure for each fleet (\"ID\" independent, \"AR\" AR(1), or \"US\" for unstructured)."
-    txt$keyCorObs <- "Coupling of correlation parameters must be specified if the AR(1) structure is chosen above."
+    txt$keyCorObs <- paste0("Coupling of correlation parameters can only be specified if the AR(1) structure is chosen above.",
+                            paste0("\n#",paste0(colnames(fit$conf$keyCorObs), collapse=" ")))
     txt$stockRecruitmentModelCode <- "Stock recruitment code (0 for plain random walk, 1 for Ricker, and 2 for Beverton-Holt)."
     txt$noScaledYears <- "Number of years where catch scaling is applied."
     txt$keyScaledYears <- "A vector of the years where catch scaling is applied."
