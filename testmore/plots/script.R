a350d50b485759673af4a6a2a43f195e799d2d1c
Christoffer Moesgaard Albertsen <xpv919@alumni.ku.dk>
2018-01-03 10:35:29 +0100

    In testmore/plots; new call to pdf for each plot

--- testmore/plots/script.R
+++ testmore/plots/script.R
@@ -23,52 +23,70 @@ retro <- retro(fit,year=3,ncores=1)
 td <- tempdir()
 f <- file.path(td,"SAMplot_%03d.pdf")
 
+srplot0<-function(fit, ...){
+  X <- summary(fit)
+  n<-nrow(X)
+  lag <- fit$conf$minAge
+  idxR <- (lag+1):n
+  idxS <- 1:(n-lag)
+  R<-X[idxR,1]
+  S<-X[idxS,4]
+  Rnam<-colnames(X)[1]
+  Snam<-colnames(X)[4]
+  y<-rownames(X)
+  plot(S,R, xlab=Snam, ylab=Rnam, type="l", xlim=range(0,S), ylim=range(0,R), ...)
+  text(S,R, labels=y[idxR], cex=.7, col="red" )
+}
 
-pdf(f,
-    encoding="ISOLatin1",
-    useDingbats=FALSE,
-    compress=FALSE,
-    onefile=FALSE,
-    paper="a4",
-    colormodel="cmyk",
-    useKerning=FALSE,
-    version="1.4")
-## Calibration plot - if this changes something else is wrong
-plot(1:10,col=c("black","red",rgb(0.5,0.9,0.1,0.700000)))
-## sam plots
-plot(fit)
-corplot(fit)
-fbarplot(fit)
-ssbplot(fit)
-tsbplot(fit)
-recplot(fit)
-catchplot(fit)
-parplot(fit)
-par(mfrow=c(1,1))
-srplot(fit)
-fitplot(fit)
-## samset plots
-par(mfrow=c(1,1))
-fbarplot(retro)
-ssbplot(retro)
-tsbplot(retro)
-recplot(retro)
-catchplot(retro)
-parplot(retro)
-## samforecast plots
-par(mfrow=c(1,1))
-plot(forec)
-fbarplot(forec)
-ssbplot(forec)
-tsbplot(forec)
-recplot(forec)
-catchplot(forec)
-## samres plots
-plot(resid)
-corplot(resid)
-## Process residuals
-plot(resp)
-dev.off()
+txt <- c(
+    ## Calibration plot - if this changes something else is wrong
+    'plot(1:10,col=c("black","red",rgb(0.5,0.9,0.1,0.700000)))',
+    ## sam plots
+    'plot(fit)',
+    'corplot(fit)',
+    'fbarplot(fit)',
+    'ssbplot(fit)',
+    'tsbplot(fit)',
+    'recplot(fit)',
+    'catchplot(fit)',
+    'parplot(fit)',
+    'srplot(fit)',
+    'srplot0(fit)',
+    'fitplot(fit)',
+    ## samset plots
+    'fbarplot(retro)',
+    'ssbplot(retro)',
+    'tsbplot(retro)',
+    'recplot(retro)',
+    'catchplot(retro)',
+    'parplot(retro)',
+    ## samforecast plots
+    'plot(forec)',
+    'fbarplot(forec)',
+    'ssbplot(forec)',
+    'tsbplot(forec)',
+    'recplot(forec)',
+    'catchplot(forec)',
+    ## samres plots
+    'plot(resid)',
+    'corplot(resid)',
+    ## Process residuals
+    'plot(resp)'
+     )
+
+for(i in 1:length(txt)){
+    pdf(sprintf(f,i),
+        encoding="ISOLatin1",
+        useDingbats=FALSE,
+        compress=FALSE,
+        onefile=TRUE,
+        paper="a4",
+        colormodel="cmyk",
+        useKerning=FALSE,
+        version="1.4")
+    eval(parse(text = txt[i]))
+    dev.off()
+}
 
 lf <- list.files(td,pattern="^SAMplot_[[:digit:]]{3}.pdf$",full.names=TRUE)
 
