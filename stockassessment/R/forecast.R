d679586e375062d1497374f0b08f9ca4d3498499
Anders Nielsen <fishfollower@gmail.com>
2018-01-20 11:35:40 +0100

    doc

--- stockassessment/R/forecast.R
+++ stockassessment/R/forecast.R
@@ -36,6 +36,8 @@ rmvnorm <- function(n = 1, mu, Sigma){
 ##' @param label optional label to appear in short table
 ##' @param overwriteSelYears if a vector of years is specified, then the average selectivity of those years is used (not recommended)
 ##' @param deterministic option to turn all process noise off (not recommended, as it will likely cause bias)
+##' @param cf.cv.keep.cv exotic option 
+##' @param cf.cv.keep.fv exotic option
 ##' @details There are three ways to specify a scenario. If e.g. four F values are specified (e.g. fval=c(.1,.2,.3,4)), then the first value is used in the last assessment year (base.year), and the three following in the three following years. Alternatively F's can be specified by a scale, or a target catch. Only one option can be used per year. So for instance to set a catch in the first year and an F-scale in the following one would write catchval=c(10000,NA,NA,NA), fscale=c(NA,1,1,1). The length of the vector specifies how many years forward the scenarios run. 
 ##' @return an object of type samforecast
 ##' @importFrom stats median uniroot quantile
@@ -267,7 +269,7 @@ forecast <- function(fit, fscale=NULL, catchval=NULL, fval=NULL, nosim=1000, yea
       ii <- which(apply(rbind(cv,cf),2,function(x)any(!is.na(x))))
       theta <- rep(1,length(ii)+1)
       if(length(theta)>noCatchFleets)stop("Over-specified in cf.cv.keep.cv")
-      fun <- function(th){
+      lsfun <- function(th){
         s <- rep(NA,noCatchFleets)
         s[ii] <- th[1:length(ii)]
         s[-ii] <- th[length(ii)+1]
@@ -282,7 +284,7 @@ forecast <- function(fit, fscale=NULL, catchval=NULL, fval=NULL, nosim=1000, yea
         med <- c(medcv/sum(medcv),medcv,mean(apply(simcat,2,sum)))
         return(sum(((cfcvtcv-med)/cfcvtcv)^2, na.rm=TRUE))
       }
-      ff <- nlminb(theta,fun, lower=0.001, upper=1000)
+      ff <- nlminb(theta,lsfun, lower=0.001, upper=1000)
       sim <- simtmp
     }
     
