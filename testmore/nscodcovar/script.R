631fe00238022f00fd96376ddf7d442c3e6aebbb
olavbr <olavbr@nr.no>
2019-03-13 18:33:06 +0100

    Add testmore

--- testmore/nscodcovar/script.R
+++ testmore/nscodcovar/script.R
@@ -12,7 +12,6 @@ sw<-read.ices("sw.dat")
 surveys<-read.ices("survey.dat")
 
 ## Estimate a correlation 
-
 dat<-setup.sam.data(surveys=surveys,
                     residual.fleet=cn, 
                     prop.mature=mo, 
@@ -30,12 +29,13 @@ conf$fbarRange <- c(2,4)
 conf$obsCorStruct[] <- c("ID","ID","AR")
 conf$keyCorObs[3,1:3] <- 0
 
+conf$keyVarObs[3,] = c(2,3,4,5,-1,-1)
 par<-defpar(dat,conf)
 fit<-sam.fit(dat,conf,par)
 
 ## assign same correlation 
-
 estcor<-cov2cor(fit$rep$obsCov[[3]])
+surveys<-read.ices("survey.dat")
 attr(surveys[[2]], "cor") <- lapply(1:23, function(i)estcor)
 
 dat<-setup.sam.data(surveys=surveys,
@@ -52,13 +52,16 @@ dat<-setup.sam.data(surveys=surveys,
 
 conf<-defcon(dat)
 conf$fbarRange <- c(2,4)
+conf$obsCorStruct[] <- c("ID","ID","ID")
+conf$keyVarObs[3,] = c(2,3,4,5,-1,-1)
+
 par<-defpar(dat,conf)
 fit2<-sam.fit(dat,conf,par)
 
 
 ## assign same covariance
-
 estcov<-fit$rep$obsCov[[3]]
+surveys<-read.ices("survey.dat")
 attr(surveys[[2]], "cov") <- lapply(1:23, function(i)estcov)
 
 dat<-setup.sam.data(surveys=surveys,
@@ -76,14 +79,17 @@ dat<-setup.sam.data(surveys=surveys,
 conf<-defcon(dat)
 conf$fbarRange <- c(2,4)
 conf$fixVarToWeight <- 1
+conf$keyVarObs[3,] = c(2,2,2,2,-1,-1)
+conf$obsCorStruct[] <- c("ID","ID","ID")
 par<-defpar(dat,conf)
-fit3<-sam.fit(dat,conf,par,rm.unidentified=TRUE)
 
-
+par$logSdLogObs = c(-0.35,-0.35,0)
+map = list(logSdLogObs = as.factor(c(0,1,NA)))
+fit3<-sam.fit(dat,conf,par,map = map)
 
 ## assign same with cov-weight. Here the corelation is fixed, but inverse variance is used as relative weight only.
-
 estcov<-fit$rep$obsCov[[3]]
+surveys<-read.ices("survey.dat")
 attr(surveys[[2]], "cov-weight") <- lapply(1:23, function(i)estcov)
 
 dat<-setup.sam.data(surveys=surveys,
@@ -100,6 +106,8 @@ dat<-setup.sam.data(surveys=surveys,
 
 conf<-defcon(dat)
 conf$fbarRange <- c(2,4)
+conf$keyVarObs[3,] = c(2,2,2,2,-1,-1)
+conf$obsCorStruct[] <-  c("ID","ID","ID") 
 par<-defpar(dat,conf)
 fit4<-sam.fit(dat,conf,par)
 
