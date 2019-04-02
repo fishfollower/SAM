db98a33eab870dddd305a0cca0921e27320936d9
olavbr <olavbr@nr.no>
2019-04-02 15:51:30 +0200

    Minor change in U in F

--- stockassessment/inst/include/f.hpp
+++ stockassessment/inst/include/f.hpp
@@ -187,7 +187,7 @@ Type nllFseparable(confSet &conf, paraSet<Type> &par, array<Type> &logF, array<T
     nll += nldens(diff);
   }
   for(int y=1; y<timeSteps; ++y){
-    nll += -dnorm(logV(y),rhoV* logV(y-1) - par.sepFalpha(par.sepFalpha.size()-1) ,sdV(0),true);
+    nll += -dnorm(logV(y),rhoV* logV(y-1) + par.sepFalpha(par.sepFalpha.size()-1) ,sdV(0),true);
   }
   
   //Accomondate for that logU and logV is a transformation of logF
