27b689fb504115044517f7b885133736876815ca
fishfollower <fishfollower@gmail.com>
2019-06-13 13:57:11 +0200

    doc

--- docs/defpar.md
+++ docs/defpar.md
@@ -17,13 +17,13 @@ defpar(dat, conf)
 
 Argument      |Description
 ------------- |----------------
-```dat```     |     sam data object as returned from the function setup.sam.data
-```conf```     |     sam configuration list, which could be read from a configuration file via the `loadConf` command, or a default/dummy configuration can be generated via the `defcon` function
+```dat```     |     sam data object as returned from the function `setup.sam.data`
+```conf```     |     sam configuration list, which could be read from a configuration file via the `loadConf` function. A default/dummy configuration can be generated via the `defcon` function.
 
 ## Details
 
 
- The model parameters and random effects are not initialized in any clever way most are simply set to zero. If convergence problems occour different initial values can be tested, but it is more likely the problem is the model configuration.
+ The model parameters and random effects are not initialized in any clever way - most are simply set to zero. If convergence problems occour different initial values can be tested, but it is more likely a problem with the model configuration.
 
 
 ## Value
--- stockassessment/R/init.R
+++ stockassessment/R/init.R
@@ -1,7 +1,7 @@
 ##' Setup initial values for all model parameters and random effects.
-##' @param dat sam data object as returned from the function setup.sam.data
-##' @param conf sam configuration list, which could be read from a configuration file via the \code{loadConf} command, or a default/dummy configuration can be generated via the \code{defcon} function   
-##' @details The model parameters and random effects are not initialized in any clever way most are simply set to zero. If convergence problems occour different initial values can be tested, but it is more likely the problem is the model configuration. 
+##' @param dat sam data object as returned from the function \code{setup.sam.data}
+##' @param conf sam configuration list, which could be read from a configuration file via the \code{loadConf} function. A default/dummy configuration can be generated via the \code{defcon} function.   
+##' @details The model parameters and random effects are not initialized in any clever way - most are simply set to zero. If convergence problems occour different initial values can be tested, but it is more likely a problem with the model configuration. 
 ##' @return a list containing initial values for all model parameters and random effects in the model.
 ##' @export
 defpar <- function(dat,conf){
