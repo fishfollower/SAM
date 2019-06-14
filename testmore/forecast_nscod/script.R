3334c24affe8908d64704ac8b110ad185e415a70
Christoffer Moesgaard Albertsen <ca@calbertsen.dk>
2019-06-14 14:27:15 +0200

    Cpp forecast tests

---
+++ testmore/forecast_nscod/script.R
@@ -0,0 +1,90 @@
+library(stockassessment)
+cn<-read.ices("../nscod/cn.dat")
+cw<-read.ices("../nscod/cw.dat")
+dw<-read.ices("../nscod/dw.dat")
+lf<-read.ices("../nscod/lf.dat")
+lw<-read.ices("../nscod/lw.dat")
+mo<-read.ices("../nscod/mo.dat")
+nm<-read.ices("../nscod/nm.dat")
+pf<-read.ices("../nscod/pf.dat")
+pm<-read.ices("../nscod/pm.dat")
+sw<-read.ices("../nscod/sw.dat")
+surveys<-read.ices("../nscod/survey.dat")
+
+dat<-setup.sam.data(surveys=surveys,
+                    residual.fleet=cn, 
+                    prop.mature=mo, 
+                    stock.mean.weight=sw, 
+                    catch.mean.weight=cw, 
+                    dis.mean.weight=dw, 
+                    land.mean.weight=lw,
+                    prop.f=pf, 
+                    prop.m=pm, 
+                    natural.mortality=nm, 
+                    land.frac=lf)
+
+conf<-defcon(dat)
+conf$keyLogFsta[1,] <- c(0, 1, 2, 3, 4, 5)
+conf$corFlag <- 2
+conf$keyLogFpar <- matrix(
+         c(-1, -1, -1, -1, -1, -1,
+            0,  1,  2,  3,  4, -1,
+            5,  6,  7,  8, -1, -1
+           ), nrow=3, byrow=TRUE)
+conf$keyVarF[1,] <- c(0, 1, 1, 1, 1, 1)
+conf$keyVarObs <- matrix(
+         c( 0,  1,  2,  2,  2,  2,
+            3,  4,  4,  4,  4, -1,
+            5,  6,  6,  6, -1, -1
+           ), nrow=3, byrow=TRUE)
+conf$noScaledYears <- 13
+conf$keyScaledYears <- 1993:2005
+conf$keyParScaledYA <- row(matrix(NA, nrow=13, ncol=6))-1
+conf$fbarRange <- c(2,4)
+
+par<-defpar(dat,conf)
+fit<-sam.fit(dat,conf,par)
+
+
+tol <- 1e-6
+## Test fscale
+target <- c(0.9,0.9,0.9,1.1,1.1,1.1,1,1,1)
+fv <- forecast(fit, fscale = target, biasCorrect = FALSE)
+cat("fscale tab:\t",paste(all.equal(tail(fbartable(fit)[,1],1) * cumprod(c(1,target)),
+                     attr(fv,"tab")[,"fbar:median"], check.attributes = FALSE, tolerance = tol), collapse = ", "),"\n", file="res.out")
+cat("fscale fulltab:\t",paste(all.equal(c(head(fbartable(fit)[,1],-1),tail(fbartable(fit)[,1],1) * cumprod(c(1,target))),
+                                        attr(fv,"fulltab")[,"fbar:median"], check.attributes = FALSE, tolerance = tol), collapse = ", "),"\n", file="res.out", append = TRUE)
+
+
+## Test fval
+target <- c(0.1,0.2,0.3,0.1)
+fv <- forecast(fit, fval = target, biasCorrect = FALSE)
+cat("fval tab:\t",paste(all.equal(c(tail(fbartable(fit)[,1],1), target),
+                 attr(fv,"tab")[,"fbar:median"], check.attributes = FALSE, tolerance = tol)),"\n", file="res.out", append = TRUE) 
+cat("fval fulltab:\t",paste(all.equal(c(head(fbartable(fit)[,1],-1),tail(fbartable(fit)[,1],1), target),
+                     attr(fv,"fulltab")[,"fbar:median"], check.attributes = FALSE, tolerance = tol), collapse = ", "),"\n", file="res.out", append = TRUE)
+
+## Test catchval
+target <- round(c(0.5,0.3) * mean(tail(rep(catchtable(fit)[,1],4))))
+fv <- forecast(fit, catchval = target, biasCorrect = FALSE)
+cat("catchval tab:\t",paste(all.equal(target,
+                     round(attr(fv,"tab")[-1,"catch:median"]), check.attributes = FALSE, tolerance = tol), collapse = ", "),"\n", file="res.out", append = TRUE) 
+cat("catchval fulltab:\t",paste(all.equal(head(catchtable(fit)[,1],-1),
+                     attr(fv,"fulltab")[1:(nrow(catchtable(fit))-1),"catch:median"], check.attributes = FALSE, tolerance = tol), collapse = ", "),"\n", file="res.out", append = TRUE)
+
+## Test landval
+target <- round(c(0.1,0.1) * mean(tail(rep(catchtable(fit)[,1],4))))
+fv <- forecast(fit, landval = target, biasCorrect = FALSE, splitLD = TRUE)
+cat("landval tab:\t",paste(all.equal(target,
+                     round(attr(fv,"tab")[-1,"Land:median"]), check.attributes = FALSE, tolerance = tol), collapse = ", "),"\n", file="res.out", append = TRUE) 
+
+
+## Test fmodel
+target <- rep(NA,5)
+fv <- forecast(fit, landval = target, biasCorrect = FALSE)
+cat("fmodel tab:\t",paste(all.equal(rep(tail(fbartable(fit)[,1],1),length(target)),
+                     attr(fv,"tab")[-1,"fbar:median"], check.attributes = FALSE, tolerance = tol), collapse = ", "),"\n", file="res.out", append = TRUE) 
+cat("fmodel tab:\t",paste(all.equal(c(fbartable(fit)[,1],rep(tail(fbartable(fit)[,1],1),length(target))),
+                     attr(fv,"fulltab")[,"fbar:median"], check.attributes = FALSE, tolerance = tol), collapse = ", "),"\n", file="res.out", append = TRUE) 
+
+
---
+++ testmore/forecast_nsher/script.R
@@ -0,0 +1,82 @@
+library(stockassessment)
+cn<-read.ices("../nsher/cn.dat")
+cw<-read.ices("../nsher/cw.dat")
+dw<-read.ices("../nsher/dw.dat")
+lf<-read.ices("../nsher/lf.dat")
+lw<-read.ices("../nsher/lw.dat")
+mo<-read.ices("../nsher/mo.dat")
+nm<-read.ices("../nsher/nm.dat")
+pf<-read.ices("../nsher/pf.dat")
+pm<-read.ices("../nsher/pm.dat")
+sw<-read.ices("../nsher/sw.dat")
+surveys<-read.ices("../nsher/survey.dat")
+
+dat<-setup.sam.data(surveys=surveys,
+                    residual.fleet=cn, 
+                    prop.mature=mo, 
+                    stock.mean.weight=sw, 
+                    catch.mean.weight=cw, 
+                    dis.mean.weight=dw, 
+                    land.mean.weight=lw,
+                    prop.f=pf, 
+                    prop.m=pm, 
+                    natural.mortality=nm, 
+                    land.frac=lf)
+
+conf<-defcon(dat)
+
+conf$fbarRange<-c(2,6)
+conf$corFlag<-1
+conf$keyLogFpar<-matrix(c(
+-1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,
+-1,    0,    1,    2,    3,    4,    5,    6,   -1,
+-1,    7,   -1,   -1,   -1,   -1,   -1,   -1,   -1,
+ 8,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1), nrow=4, byrow=TRUE)
+
+par<-defpar(dat,conf)
+par$logFpar<-rep(0,9)
+
+fit<-sam.fit(dat,conf,par)
+
+tol <- 1e-6
+## Test fscale
+target <- c(0.9,0.9,0.9,1.1,1.1,1.1,1,1,1)
+fv <- forecast(fit, fscale = target, biasCorrect = FALSE)
+cat("fscale tab:\t",paste(all.equal(tail(fbartable(fit)[,1],1) * cumprod(c(1,target)),
+                     attr(fv,"tab")[,"fbar:median"], check.attributes = FALSE, tolerance = tol), collapse = ", "),"\n", file="res.out")
+cat("fscale fulltab:\t",paste(all.equal(c(head(fbartable(fit)[,1],-1),tail(fbartable(fit)[,1],1) * cumprod(c(1,target))),
+                                        attr(fv,"fulltab")[,"fbar:median"], check.attributes = FALSE, tolerance = tol), collapse = ", "),"\n", file="res.out", append = TRUE)
+
+
+## Test fval
+target <- c(0.1,0.2,0.3,0.1)
+fv <- forecast(fit, fval = target, biasCorrect = FALSE)
+cat("fval tab:\t",paste(all.equal(c(tail(fbartable(fit)[,1],1), target),
+                 attr(fv,"tab")[,"fbar:median"], check.attributes = FALSE, tolerance = tol)),"\n", file="res.out", append = TRUE) 
+cat("fval fulltab:\t",paste(all.equal(c(head(fbartable(fit)[,1],-1),tail(fbartable(fit)[,1],1), target),
+                     attr(fv,"fulltab")[,"fbar:median"], check.attributes = FALSE, tolerance = tol), collapse = ", "),"\n", file="res.out", append = TRUE)
+
+## Test catchval
+target <- round(c(0.5,0.3) * mean(tail(rep(catchtable(fit)[,1],4))))
+fv <- forecast(fit, catchval = target, biasCorrect = FALSE)
+cat("catchval tab:\t",paste(all.equal(target,
+                     round(attr(fv,"tab")[-1,"catch:median"]), check.attributes = FALSE, tolerance = tol), collapse = ", "),"\n", file="res.out", append = TRUE) 
+cat("catchval fulltab:\t",paste(all.equal(head(catchtable(fit)[,1],-1),
+                     attr(fv,"fulltab")[1:(nrow(catchtable(fit))-1),"catch:median"], check.attributes = FALSE, tolerance = tol), collapse = ", "),"\n", file="res.out", append = TRUE)
+
+## Test landval
+target <- round(c(0.1,0.1) * mean(tail(rep(catchtable(fit)[,1],4))))
+fv <- forecast(fit, landval = target, biasCorrect = FALSE, splitLD = TRUE)
+cat("landval tab:\t",paste(all.equal(target,
+                     round(attr(fv,"tab")[-1,"Land:median"]), check.attributes = FALSE, tolerance = tol), collapse = ", "),"\n", file="res.out", append = TRUE) 
+
+
+## Test fmodel
+target <- rep(NA,5)
+fv <- forecast(fit, landval = target, biasCorrect = FALSE)
+cat("fmodel tab:\t",paste(all.equal(rep(tail(fbartable(fit)[,1],1),length(target)),
+                     attr(fv,"tab")[-1,"fbar:median"], check.attributes = FALSE, tolerance = tol), collapse = ", "),"\n", file="res.out", append = TRUE) 
+cat("fmodel tab:\t",paste(all.equal(c(fbartable(fit)[,1],rep(tail(fbartable(fit)[,1],1),length(target))),
+                     attr(fv,"fulltab")[,"fbar:median"], check.attributes = FALSE, tolerance = tol), collapse = ", "),"\n", file="res.out", append = TRUE) 
+
+
