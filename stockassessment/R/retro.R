6c4a26632fb82b0cbb7f9f6fcf673bed76086a0c
fishfollower <fishfollower@gmail.com>
2021-02-12 11:13:02 +0100

    NM process part

--- stockassessment/R/retro.R
+++ stockassessment/R/retro.R
@@ -88,7 +88,7 @@ runwithout <- function(fit, year=NULL, fleet=NULL, map=fit$obj$env$map, ...){
   conf <- loadConf(data, fakefile, patch=TRUE)
   close(fakefile)
   par <- defpar(data,conf)
-  par[!names(par)%in%c("logN", "logF", "logSW", "logCW", "logitMO")]<-fit$pl[!names(fit$pl)%in%c("missing", "logN", "logF", "logSW", "logCW", "logitMO")]
+  par[!names(par)%in%c("logN", "logF", "logSW", "logCW", "logitMO", "logNM")]<-fit$pl[!names(fit$pl)%in%c("missing", "logN", "logF", "logSW", "logCW", "logitMO", "logNM")]
   ret <- sam.fit(data, conf, par, rm.unidentified=TRUE, map=map, lower=fit$low, upper=fit$hig, ...)
   return(ret)
 }
