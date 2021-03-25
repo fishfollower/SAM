5fbe4d657bc74535bf2e054fdae2caf2daa9d8c8
fishfollower <fishfollower@gmail.com>
2021-03-25 14:40:46 +0100

    doc stuff

--- stockassessment/R/forecast.R
+++ stockassessment/R/forecast.R
@@ -46,13 +46,14 @@ rmvnorm <- function(n = 1, mu, Sigma){
 ##' @param lagR if the second youngest age should be reported as recruits
 ##' @param splitLD if TRUE the result is split in landing and discards
 ##' @param addTSB if TRUE the total stock biomass (TSB) is added
+##' @param useSWmodel if TRUE the catch mean weight predicted from the assessment model is used (can only be used for configurations supporting this)
 ##' @param useCWmodel if TRUE the catch mean weight predicted from the assessment model is used (can only be used for configurations supporting this)
 ##' @param useMOmodel if TRUE the proportion mature predicted from the assessment model is used (can only be used for configurations supporting this)
 ##' @param useNMmodel if TRUE the natural mortality predicted from the assessment model is used (can only be used for configurations supporting this)
 ##' @param savesim save the individual simulations 
 ##' @details There are four ways to specify a scenario. If e.g. four F values are specified (e.g. fval=c(.1,.2,.3,4)), then the first value is used in the last assessment year (base.year), and the three following in the three following years. Alternatively F's can be specified by a scale, or a target catch. Only one option can be used per year. So for instance to set a catch in the first year and an F-scale in the following one would write catchval=c(10000,NA,NA,NA), fscale=c(NA,1,1,1). The length of the vector specifies how many years forward the scenarios run. 
 ##' @return an object of type samforecast
-##' @importFrom stats median uniroot quantile
+##' @importFrom stats median uniroot quantile plogis
 ##' @importFrom TMB sdreport MakeADFun
 ##' @export
 forecast <- function(fit, fscale=NULL, catchval=NULL, catchval.exact=NULL, fval=NULL, nextssb=NULL, landval=NULL, cwF=NULL, nosim=1000, year.base=max(fit$data$years), ave.years=max(fit$data$years)+(-4:0), rec.years=max(fit$data$years)+(-9:0), label=NULL, overwriteSelYears=NULL, deterministic=FALSE, processNoiseF=TRUE, customWeights=NULL, customSel=NULL, lagR=FALSE, splitLD=FALSE, addTSB=FALSE, useSWmodel=(fit$conf$stockWeightModel==1), useCWmodel=(fit$conf$catchWeightModel==1), useMOmodel=(fit$conf$matureModel==1), useNMmodel=(fit$conf$mortalityModel==1), savesim=FALSE){
