03c0ee008606b911afbd200c8d24f2f7cb59b561
fishfollower <an@aqua.dtu.dk>
2023-01-18 15:59:27 +0100

    copied relist fix

--- stockassessment/R/relist.R
+++ stockassessment/R/relist.R
@@ -8,14 +8,14 @@ relist.array <- function (flesh, skeleton = attr(flesh, "skeleton"))
         return(array(0,dim=d, dimnames = dimnames(skeleton)))
     if (is.numeric(skeleton[1])) 
         return(array(flesh, dim=d, dimnames = dimnames(skeleton)))
-    stop("Only implemented for numeric arrays")
-    ## result <- skeleton
-    ## ind <- 1L
-    ## for (i in seq_along(skeleton)) {
-    ##     size <- length(unlist(skeleton[[i, j]]))
-    ##     result[[i, j]] <- relist(flesh[seq.int(ind, length.out = size)], 
-    ##         skeleton[[i, j]])
-    ##     ind <- ind + size
-    ## }
-    ## result
+    ## stop("Only implemented for numeric arrays")
+    result <- skeleton
+    ind <- 1L
+    for (i in seq_along(skeleton)) {
+        size <- length(unlist(skeleton[[i]]))
+        result[[i]] <- relist(flesh[seq.int(ind, length.out = size)], 
+            skeleton[[i]])
+        ind <- ind + size
+    }
+    result
 }
