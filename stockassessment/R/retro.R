2a689b78f8673b4490e7f261c78fb3511018651c
Vanessa Trijoulet <vtri@dtu.dk>
2025-02-26 14:54:12 +0100

    Update retro.R
    typo in year argument retro function

--- stockassessment/R/retro.R
+++ stockassessment/R/retro.R
@@ -127,7 +127,7 @@ runwithout.sam <- function(fit, year=NULL, fleet=NULL, map=fit$obj$env$map, ...)
 
 ##' retro run 
 ##' @param fit a fitted model object as returned from sam.fit
-##' @param year either 1) a single integer n in which case runs where all fleets are reduced by 1, 2, ..., n are returned, 2) a vector of years in which case runs where years from and later are excluded for all fleets, and 3 a matrix of years were each column is a fleet and each column corresponds to a run where the years and later are excluded.    
+##' @param year either 1) a single integer n in which case runs where all fleets are reduced by 1, 2, ..., n are returned, 2) a vector of years in which case runs where years from and later are excluded for all fleets, and 3 a matrix of years were each column is a fleet and each row corresponds to a run where the years and later are excluded.    
 ##' @param ncores the number of cores to attempt to use
 ##' @param ... extra arguments to \code{\link{sam.fit}}
 ##' @details ...
